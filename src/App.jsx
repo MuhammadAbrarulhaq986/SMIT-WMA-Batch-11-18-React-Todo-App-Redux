d5b5d36fa7089f49b5d4a41fd519b88543296774
Muhammad Abrar ul haq <ulhaqabrar106@gmail.com>
2024-09-26 22:38:37 +0500

    Todo App using Redux fix the display

--- src/App.jsx
+++ src/App.jsx
@@ -55,77 +55,78 @@ const App = () => {
   // * Return the JSX for the App component
   return (
     <>
-      <div className="container mx-auto p-auto pt-12 md:p-12 lg:p-16 shadow-md rounded">
-        <div className="max-w-full mx-auto p-14 rounded shadow-md todo">
-          <h1 className="text-3xl font-bold text-center text-white mb-4">
-            Redux Todo app
-          </h1>
-          <form className="flex flex-wrap justify-center mb-4">
-            <input
-              type="text"
-              ref={todoVal}
-              className="w-1/2 p-2 pl-10 text-sm text-black  bg-white rounded"
-              placeholder="Add a new todo..."
-              style={{ color: "black" }}
-            />
-            <button
-              onClick={addTodoInRedux}
-              className="bg-orange-500 hover:bg-orange-700 text-white font-bold text-2xl p-3  rounded ml-2"
-            >
-              Add Todo
-            </button>
-          </form>
-          <ul className="list-none mb-4">
-            {selector.length > 0 ? (
-              selector.map((item, index) => {
-                return (
-                  <li key={item.id} className="flex items-center mb-2">
-                    {item.isEditing ? (
-                      <input
-                        type="text"
-                        value={item.title}
-                        onChange={(e) => {
-                          dispatch(
-                            editTodo({
-                              id: item.id,
-                              title: e.target.value,
-                            })
-                          );
-                        }}
-                        className="w-1/2 p-2 pl-10 text-sm text-black bg-gray-100 shadow-md rounded"
-                      />
-                    ) : (
-                      <span className="text-lg">{item.title}</span>
-                    )}
-                    <button
-                      onClick={() => deleteItemFromRedux(index)}
-                      className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded ml-2"
-                    >
-                      Delete
-                    </button>
-                    <button
-                      onClick={() => {
+      <div className="todo container text-center justify-center pt-12 md:p-12 lg:p-16 text-white shadow-md rounded mx-auto">
+        <h1 className="text-3xl font-bold text-center text-white mb-4">
+          Redux Todo app
+        </h1>
+        <form className="flex flex-wrap justify-center mb-4 flex justify-center">
+          <input
+            type="text"
+            ref={todoVal}
+            className="p-4 my-4 pl-5 text-xs text-white bg-white rounded"
+            placeholder="Add a new todo..."
+            style={{ color: "black", height: "30px" }}
+          />
+          <button
+            onClick={addTodoInRedux}
+            className="bg-orange-500 hover:bg-orange-700 text-white font-bold text-base p-2  my-2 rounded ml-2"
+          >
+            Add Todo
+          </button>
+        </form>
+        <ul className="list-none mb-4 mx-auto">
+          {selector.length > 0 ? (
+            selector.map((item, index) => {
+              return (
+                <li
+                  key={item.id}
+                  className="flex items-center mb-2 justify-center"
+                >
+                  {item.isEditing ? (
+                    <input
+                      type="text"
+                      value={item.title}
+                      onChange={(e) => {
                         dispatch(
                           editTodo({
                             id: item.id,
-                            isEditing: !item.isEditing,
+                            title: e.target.value,
                           })
                         );
                       }}
-                      className="bg-blue-500 hover:bg-lightgreen-500 active:bg-green-500 text-white font-bold py-2 px-4 rounded ml-2"
-                    >
-                      {item.isEditing ? "Save" : "Edit"}
-                    </button>
-                  </li>
-                );
-              })
-            ) : (
-              <h1 className="text-2xl text-center text-white font-bold">
-                No Todos Found!
-              </h1>
-            )}
-          </ul>
-        </div>
+                      className="w-1/2 p-2 pl-10 text-sm text-white shadow-md rounded"
+                    />
+                  ) : (
+                    <span className="text-lg">{item.title}</span>
+                  )}
+                  <button
+                    onClick={() => deleteItemFromRedux(index)}
+                    className="bg-red-500 hover:bg-red-700 text-white font-bold text-base p-2  my-2 rounded ml-2"
+                  >
+                    Delete
+                  </button>
+                  <button
+                    onClick={() => {
+                      dispatch(
+                        editTodo({
+                          id: item.id,
+                          isEditing: !item.isEditing,
+                        })
+                      );
+                    }}
+                    className="bg-blue-500 hover:bg-lightgreen-500 active:bg-green-500 text-white font-bold text-base p-2  my-2 rounded ml-2"
+                  >
+                    {item.isEditing ? "Save" : "Edit"}
+                  </button>
+                </li>
+              );
+            })
+          ) : (
+            <h1 className="text-2xl text-center text-white font-bold">
+              No Todos Found!
+            </h1>
+          )}
+        </ul>
       </div>
     </>
   );
